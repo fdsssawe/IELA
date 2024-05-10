62782649c10b74e77f22b82f478e26ea7a09ae16
Olexander Zhovanyk <mindesync@gmail.com>
2024-05-10 23:42:10 +0300

    Git innit

--- src/http/index.ts
+++ src/http/index.ts
@@ -9,6 +9,7 @@ const api = axios.create({
     baseURL: API_URL
 })
 
+
 api.interceptors.request.use((config)=>{
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config
